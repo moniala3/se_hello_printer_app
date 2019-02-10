7ac0e355a71fe75521430ce7a9b15b280b0ae59e
Moniala3 <Mgorol94@users.noreply.git.hub.com>
2019-02-10 10:43:32 +0100

    Kolejna wersja to bedzie

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,13 +4,16 @@ from formater import SUPPORTED, PLAIN
 from flask import request
 
 moje_imie = "Monika"
-msg = "Hello World!"
+msg = "Witaj Swiecie!"
 
 @app.route('/')
 def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
+    imie = request.args.get('imie')
+    if not imie:
+        imie = moje_imie
     return get_formatted(msg, moje_imie,
                          output.lower())
 
